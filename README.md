056118ec96b8713a7c8cd413041612148c94bf44
lovanshgaur <lovanshgaur@gmail.com>
2026-04-15 01:23:16 +0530

    init elvtai_0.1

---
+++ README.md
@@ -0,0 +1,4 @@
+elvt.ai - v0.1 <br>
+React-Javascript-Tailwind-Gemini
+
+[LovanshGaur](https://lovansh.me/)
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-8xl text-black-500 m-5 bold">elvt.ai</h1>
+    </>
+  )
+}
+
+export default App
